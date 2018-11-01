d4f349fa48f9811e13bd759ff834f370862dcf84
sarveshlearn27@gmail.com <sarveshlearn27@gmail.com>
2018-11-02 00:45:18 +0530

    First commit

---
+++ Hrms.Core/Models/CMResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hrms.Core.Parsers
+{
+    public enum ErrorCodes:int
+    {
+        Success,
+        Failure
+    }
+    public class CMResult
+    {
+        public ErrorCodes CMResultErrorCode { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+    }
+}
---
+++ Hrms.Core/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hrms.Core
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+
+        public List<DateTime> InTimings { get; set; }
+
+        public List<DateTime> OutTimings { get; set; }
+
+        public int LateMarks { get; set; }
+
+        public int TotalDeductions { get; set; }
+
+        public Employee()
+        {
+            if (LateMarks != 0)
+            {
+                TotalDeductions = LateMarks / 4;
+            }
+        }
+    }
+}
